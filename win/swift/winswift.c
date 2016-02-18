08231a53b52c143c08921795d2d9c482381c4cd5
C.W. Betts <computers57@hotmail.com>
2016-02-18 13:00:06 -0700

    Initial work on the "Swift" port.

---
+++ win/swift/winswift.c
@@ -0,0 +1,71 @@
+
+#include "winswift.h"
+
+/* These will be filled out by our Swift main file */
+struct window_procs swift_procs = {
+	"swift",
+	0L,
+    0L,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	genl_putmixed,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+    genl_message_menu,
+	donull,
+	donull,
+	donull,
+#ifdef CLIPPING
+	donull,
+#endif
+#ifdef POSITIONBAR
+    donull,
+#endif
+	donull,
+    // nh3d_print_glyph_compose,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+	donull,
+#ifdef CHANGE_COLOR     /* only a Mac option currently */
+    donull,
+    donull,
+#endif
+    /* other defs that really should go away (they're tty specific) */
+	donull,
+	donull,
+	donull,
+    genl_preference_update,
+	genl_getmsghistory,
+	genl_putmsghistory,
+#ifdef STATUS_VIA_WINDOWPORT
+	hup_void_ndecl,                                   /* status_init */
+	hup_void_ndecl,                                   /* status_finish */
+	genl_status_enablefield, hup_status_update,
+#ifdef STATUS_HILITES
+	genl_status_threshold,
+#endif
+#endif /* STATUS_VIA_WINDOWPORT */
+	genl_can_suspend_no,
+};
---
+++ win/swift/winswift.h
@@ -0,0 +1,4 @@
+
+#include "hack.h"
+
+extern struct window_procs swift_procs;
